a99f4f957c0350c2ed0db45dc4ff206af8cff178
Wahabkhann-dev <Demo@mawbytechnologies.com>
2026-05-25 10:03:22 -0700

    Initial commit

--- Back-End/services/emailService.js
+++ Back-End/services/emailService.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: 587,
   secure: false,
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
